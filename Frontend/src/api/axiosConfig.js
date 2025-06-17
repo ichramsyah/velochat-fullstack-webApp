bc82fb2243a16c64cdc08313bc9b5423aae05240
Ichramsyah Abdurrachman <iamabdulrahman15@gmail.com>
2025-06-17 20:37:21 +0700

    Update axiosConfig.js

--- Frontend/src/api/axiosConfig.js
+++ Frontend/src/api/axiosConfig.js
@@ -6,6 +6,9 @@ const api = axios.create({
 const api = axios.create({
   baseURL: 'http://localhost:5000',
 });
+const api = axios.create({
+  baseURL: 'http://localhost:5000',
+});
 
 
 export default api;
