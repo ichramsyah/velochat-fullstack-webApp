36cc1b5eb2501dab6ecfc9907cc67a0a3067f709
Ichramsyah Abdurrachman <iamabdulrahman15@gmail.com>
2025-06-17 20:35:38 +0700

    Update LoginPage.jsx

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -22,6 +22,14 @@ const LoginPage = () => {
         email,
         password,
       });
+      
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold text-center text-gray-800">Login VeloChat</h2>
+
+        {error && <div className="p-3 t
 
       setUserInfo(data);
       navigate('/');
