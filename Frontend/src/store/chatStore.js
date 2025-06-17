ee29c34f832eb9aced5a5d3e4e87f9157d2e9c84
Ichramsyah Abdurrachman <iamabdulrahman15@gmail.com>
2025-06-17 20:34:41 +0700

    Update chatStore.js

--- Frontend/src/store/chatStore.js
+++ Frontend/src/store/chatStore.js
@@ -1,4 +1,11 @@
-import { create } from 'zustand';
+
+const useChatStore = create((set) => ({
+  selectedChat: null,
+  setSelectedChat: (chat) => set({ selectedChat: chat }),
+}));
+
+export default useChatStore;
+import { create } from 'zustand';import { create } from 'zustand';
 
 const useChatStore = create((set) => ({
   selectedChat: null,
