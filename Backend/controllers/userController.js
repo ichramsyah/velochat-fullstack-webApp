60aeacd95084b42b371c4b77aa78c452a0213993
Ichramsyah Abdurrachman <iamabdulrahman15@gmail.com>
2025-06-18 15:13:28 +0700

    Update userController.js

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -45,6 +45,49 @@ const registerUser = async (req, res) => {
   }
 };
 
+// @desc    Mendaftarkan pengguna baru
+// @route   POST /api/users/register
+// @access  Public
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      res.status(400);
+      throw new Error('Mohon isi semua field');
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(400);
+      throw new Error('Pengguna dengan email ini sudah terdaftar');
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        profilePic: user.profilePic,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400);
+      throw new Error('Data pengguna tidak valid');
+    }
+  } catch (error) {
+    res.status(res.statusCode || 500).json({ message: error.message });
+  }
+};
+
 // @desc    Mengautentikasi / Login pengguna
 // @route   POST /api/users/login
 // @access  Public
