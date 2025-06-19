b6d62314ff0822d06092fac6cbe30cc5fdf75cce
Ichramsyah Abdurrachman <iamabdulrahman15@gmail.com>
2025-06-19 18:30:40 +0700

    Update cloudinary.js

--- Backend/config/cloudinary.js
+++ Backend/config/cloudinary.js
@@ -8,12 +8,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
 });
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true,
-});
+
 
 
 export default cloudinary;
